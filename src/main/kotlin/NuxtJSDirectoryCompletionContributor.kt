705d32108bbb6d0cdb6c430b394239b2ecb3c6e4
nise_nabe <nise.nabe@gmail.com>
2021-10-03 12:42:57 +0900

    add TODO

--- src/main/kotlin/NuxtJSDirectoryCompletionContributor.kt
+++ src/main/kotlin/NuxtJSDirectoryCompletionContributor.kt
@@ -14,6 +14,8 @@ class NuxtJSDirectoryCompletionContributor: CreateDirectoryCompletionContributor
      * https://nuxtjs.org/docs/directory-structure
      */
     override fun getVariants(directory: PsiDirectory): MutableCollection<Variant> {
+        // nuxt.config で srcDir  が設定されているとディレクトリベースも変える必要がある
+        // TODO: nuxt.config をパースする方法 or nuxt がパースした結果を取得する方法
         val result = mutableListOf<Variant>()
         result.add(Variant("assets", NuxtJSRootType.INSTANCE))
         result.add(Variant("components", NuxtJSRootType.INSTANCE))
