24b5bf82f66cb8b7532393fc26b8434b48c4ac25
nise_nabe <nise.nabe@gmail.com>
2021-11-24 09:01:11 +0900

    oops

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
         exclusiveContent {
             forRepository {
                 maven {
-                    name = "gitHub
+                    name = "gitHub"
                     url = uri("https://maven.pkg.github.com/nise-nabe/gradle-plugins")
                     credentials(PasswordCredentials::class)
                 }
@@ -19,7 +19,7 @@ pluginManagement {
 
     plugins {
         kotlin("jvm") version "1.5.31"
-        id("org.jetbrains.intellij") version "1.1.6"
+        id("org.jetbrains.intellij") version "1.3.0"
         id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.1"
     }
 }
