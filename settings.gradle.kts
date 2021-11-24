05a22c79ad7c9a30a875de8f4392c74d9d5045d3
nise_nabe <nise.nabe@gmail.com>
2021-11-24 09:01:53 +0900

    add convension plugin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,5 +21,6 @@ pluginManagement {
         kotlin("jvm") version "1.5.31"
         id("org.jetbrains.intellij") version "1.3.0"
         id("org.jetbrains.gradle.plugin.idea-ext") version "1.1.1"
+        id("com.nisecoder.idea-ext-ext") version "0.0.8"
     }
 }
